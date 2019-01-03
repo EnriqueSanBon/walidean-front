eba9c0cc22c5fc9bf4571debff58afbcc2f45f87
unknown <enriquesanchezbonet@gmail.com>
2019-01-03 15:12:28 +0100

    Document Storage, alerts

--- src/components/Chicfy.vue
+++ src/components/Chicfy.vue
@@ -6,7 +6,7 @@
 
         <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" list-type="picture" :limit="2" :on-success="fileUpload" :auto-upload="false" ref="upload" :on-progress="handleProgress">
           <el-button slot="trigger" size="small" type="primary">Clic para subir archivo</el-button>
-          <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">Cargar al servidor</el-button>
+          <!--<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">Cargar al servidor</el-button>-->
         </el-upload>
 
 
@@ -166,10 +166,10 @@
   </el-container>
   <el-footer>
     <el-row type="flex" justify="center">
-      <el-button type="primary" @click="onSubmit('form2')" :disabled="disableButtonSend">Enviar</el-button>
-      <el-button type="primary" v-if="validationMethod==walideanNotAllowed" @click="onSubmit('form')">Enviar</el-button>
-      <el-button type="primary" v-if="  validationMethod==walideanAllowed" @click="onSubmit('form2')">Enviar 2</el-button>
-      <el-button @click="resetForm('form')">Reset</el-button>
+      <el-button type="primary" v-if="validationMethod==walideanNotAllowed" @click="onSubmit('form')" :disabled="disableButtonSend">Enviar</el-button>
+      <el-button type="primary" v-if="validationMethod==walideanAllowed" @click="onSubmit('form2')" :disabled="disableButtonSend">Enviar</el-button>
+      <el-button v-if="validationMethod==walideanNotAllowed" @click="resetForm('form')">Reset</el-button>
+      <el-button v-if="validationMethod==walideanAllowed" @click="resetForm('form2')">Reset</el-button>
     </el-row>
   </el-footer>
 
@@ -193,7 +193,7 @@ body {
 import axios from 'axios';
 import texts from '../texts.js';
 import consts from '../consts.js';
-import firebase from 'firebase'
+import firebase from 'firebase';
 export default {
   data() {
     var checkDNI = (rule, value, callback) => {
@@ -225,6 +225,7 @@ export default {
         }
       }, 1000);
     };
+
     return {
       form: {
         surname1: null,
@@ -289,9 +290,8 @@ export default {
       walideanAllowed: consts.walideanAllowed,
       walideanNotAllowed: consts.walideanNotAllowed,
       db: firebase.database(),
+      isMounted: false,
       controlDigits: ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
-
-
     }
   },
   methods: {
@@ -320,8 +320,9 @@ export default {
       this.$refs[form].validate((valid) => {
         if (valid) {
           var ref = this.db.ref("/users");
-          var db = this.db;
           form = this.form;
+          //var openSendDataAlert = this.openSendDataAlert();
+          var context = this;
           /*
                     ref.orderByChild("email").equalTo(this.form.email).once("value", function(snapshot) {
                       console.log("Email encontrado");
@@ -338,24 +339,30 @@ export default {
                   ref.orderByChild("email").equalTo(form.email).once("value")
                     .then(function(snapshot) {
                       if (snapshot.val() == null) {
-                        console.log("Usuario No Encontrado");
-                        db.ref('/users')
-                          .push({
-                            numberId: form.numberId,
-                            email: form.email,
-                            validated: true,
-                          })
-                        console.log("Usuario añadido");
+                        context.openSendDataAlert();
+                      } else {
+                        console.log("Email ya existe");
+                        context.$message({
+                          type: 'error',
+                          message: 'El Email introducido ya existe'
+                        });
                       }
-                      console.log("Email ya existe");
                     });
 
                 } else {
                   console.log("DNI ya existente");
+                  context.$message({
+                    type: 'error',
+                    message: 'El DNI introducido ya existe'
+                  });
                 }
               });
         } else {
           console.log('error submit!!');
+          context.$message({
+            type: 'error',
+            message: 'Error al enviar'
+          });
           return false;
         }
       });
@@ -369,19 +376,79 @@ export default {
     submitUpload() {
       console.log("Se va a subir archivo");
       console.log(this.$refs.upload.uploadFiles);
-      this.$refs.upload.uploadFiles.forEach(function(element) {
+      var form = this.form
+      this.$refs.upload.uploadFiles.forEach(function(element, index) {
         console.log("Variable del tipo:");
         console.log(typeof(element));
         console.log(element);
+        console.log(typeof(element.raw));
+        console.log(element.raw);
+        var file = element.raw // use the Blob or File API
+        var storageRef = firebase.storage().ref();
+        var mountainsRef = storageRef.child('users/' + form.numberId + '/' + index + '.jpg');
+        mountainsRef.put(file).then(function(snapshot) {
+          console.log('Uploaded a blob or file!');
+        });
       });
 
       //this.$refs.upload.submit();
+    },
+    openSendDataAlert(form) {
+      var ref = this.db.ref("/users");
+      var db = this.db;
+      form = this.form;
+      var uploadedFiles = this.$refs.upload.uploadFiles
+      this.$confirm('Vas a subir tus datos, seguro que son correctos?', 'Warning', {
+        confirmButtonText: 'SI!',
+        cancelButtonText: 'Cancelar',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: 'Autenticación realizada con exito!'
+        });
+        console.log("NEPE");
+        console.log("Usuario No Encontrado");
+        db.ref('/users')
+          .push({
+            numberId: form.numberId,
+            email: form.email,
+            validated: true,
+          })
+        console.log("Usuario añadido");
+
+        console.log("Se va a subir archivo");
+        console.log(uploadedFiles);
+        uploadedFiles.forEach(function(element, index) {
+          console.log("Variable del tipo:");
+          console.log(typeof(element));
+          console.log(element);
+          console.log(typeof(element.raw));
+          console.log(element.raw);
+          var file = element.raw // use the Blob or File API
+          var storageRef = firebase.storage().ref();
+          var mountainsRef = storageRef.child('users/' + form.numberId + '/' + index + '.jpg');
+          mountainsRef.put(file).then(function(snapshot) {
+            console.log('Uploaded a blob or file!');
+          });
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Identificación cancelada'
+        });
+      });
     }
   },
   computed: {
     disableButtonSend: function() {
-      return (this.filesUploaded.length < 2 || (this.termsAccepted == false && this.validationMethod == this.walideanAllowed) || this.form.numberId.length == 0)
+      if (!this.isMounted)
+        return;
+      return ((this.termsAccepted == false && this.validationMethod == this.walideanAllowed) || this.$refs.upload.uploadFiles.length < 2);
     }
+  },
+  mounted() {
+    this.isMounted = true;
   }
 }
 </script>
